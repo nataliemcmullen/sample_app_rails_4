b6760b8d2a02c1cd7fad1d395b41e31bd93b9da8
Natalie <natalie@Natalies-MBP.hsd1.ut.comcast.net>
2016-04-16 10:38:28 -0600

    Heroku fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
@@ -21,6 +9,9 @@ Pusher.log = function(message) {
 };
 
 $().ready(function() {
+    var pusher = new Pusher('210e48ecc2617b2f1809', {
+    });
+
     channel = pusher.subscribe('presence-demo');
     channel.bind('pusher:subscription_succeeded', function(members) {
         if (channel.members.count > 3) {
